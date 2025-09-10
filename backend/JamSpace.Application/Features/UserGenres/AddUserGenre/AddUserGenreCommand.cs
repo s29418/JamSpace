5e0081992ee957bdddd61ff802b95225b896cdc0
Mision-i <misionn5@gmail.com>
2025-09-10 14:47:48 +0200

    backend: added AddUserGenreHandler

---
+++ backend/JamSpace.Application/Features/UserGenres/AddUserGenre/AddUserGenreCommand.cs
@@ -0,0 +1,6 @@
+﻿using JamSpace.Application.Features.UserGenres.DTOs;
+using MediatR;
+
+namespace JamSpace.Application.Features.UserGenres.AddUserGenre;
+
+public record AddUserGenreCommand(Guid UserId, string GenreName) : IRequest<UserGenreDto>;
---
+++ backend/JamSpace.Application/Features/UserGenres/AddUserGenre/AddUserGenreHandler.cs
@@ -0,0 +1,33 @@
+﻿using JamSpace.Application.Common.Interfaces;
+using JamSpace.Application.Features.UserGenres.DTOs;
+using MediatR;
+
+namespace JamSpace.Application.Features.UserGenres.AddUserGenre;
+
+public class AddUserGenreHandler : IRequestHandler<AddUserGenreCommand, UserGenreDto>
+{
+    private readonly IUserGenreRepository _repo;
+    private readonly IGenreRepository _genreRepo;
+    
+    public AddUserGenreHandler(IUserGenreRepository repo, IGenreRepository genreRepo)
+    {
+        _repo = repo;
+        _genreRepo = genreRepo;
+    }
+
+    public async Task<UserGenreDto> Handle(AddUserGenreCommand request, CancellationToken ct)
+    {
+        var genre = await _genreRepo.GetGenreByNameAsync(request.GenreName, ct) 
+            ?? await _genreRepo.CreateGenreAsync(request.GenreName, ct);
+        
+        if(await _repo.UserHasGenreAsync(request.UserId, genre.Id, ct))
+            throw new InvalidOperationException("User already has this genre.");
+        
+        await _repo.AddUserGenreAsync(request.UserId, genre.Id, ct);
+        return new UserGenreDto
+        {
+            GenreId = genre.Id,
+            GenreName = genre.Name
+        };
+    }
+}

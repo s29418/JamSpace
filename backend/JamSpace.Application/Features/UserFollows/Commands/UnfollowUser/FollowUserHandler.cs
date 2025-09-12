9123bfe12c4e9a48178f1e316aff567e0c600dba
Mision-i <misionn5@gmail.com>
2025-09-12 20:25:09 +0200

    backend: added UnfollowUserHandler

--- backend/JamSpace.Application/Features/UserFollows/Commands/UnfollowUser/FollowUserHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace JamSpace.Application.Features.UserFollows.Commands.UnfollowUser;
-
-public class FollowUserHandler
-{
-    
-}
---
+++ backend/JamSpace.Application/Features/UserFollows/Commands/UnfollowUser/UnfollowUserHandler.cs
@@ -0,0 +1,28 @@
+﻿using JamSpace.Application.Common.Exceptions;
+using JamSpace.Application.Common.Interfaces;
+using MediatR;
+
+namespace JamSpace.Application.Features.UserFollows.Commands.UnfollowUser;
+
+public class UnfollowUserHandler : IRequestHandler<UnfollowUserCommand, Unit>
+{
+    private readonly IUserFollowRepository _userFollowRepository;
+    
+    public UnfollowUserHandler(IUserFollowRepository userFollowRepository)
+    {
+        _userFollowRepository = userFollowRepository;
+    }
+    
+    public async Task<Unit> Handle(UnfollowUserCommand request, CancellationToken cancellationToken)
+    {
+        bool follows = await _userFollowRepository.UserFollowsAsync(request.FollowerId, request.FollowedId, cancellationToken);
+        if (!follows)
+        {
+            throw new ConflictException("You are not following this user.");
+        }
+
+        await _userFollowRepository.UnfollowUserAsync(request.FollowerId, request.FollowedId, cancellationToken);
+        
+        return Unit.Value;
+    }
+}

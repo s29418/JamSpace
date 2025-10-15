620d461c2fbfbfb19d822c5aac8d420aad215769
s29418 <s29418@pjwstk.edu.pl>
2025-10-15 08:37:02 +0200

    backend: added DeleteUserHandler

---
+++ backend/JamSpace.Application/Features/Users/Commands/Delete/DeleteUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace JamSpace.Application.Features.Users.Commands.Delete;
+
+public sealed record DeleteUserCommand(
+    Guid UserId,
+    byte[] RowVersion 
+) : IRequest<Unit>;

7a75941d565bc51e37f23a3104567d4d6985c298
s29418 <s29418@pjwstk.edu.pl>
2026-03-28 16:04:10 +0100

    add screenshots

---
+++ backend/JamSpace.Infrastructure/Migrations/20260327133233_DemoDbInitSync.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JamSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DemoDbInitSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastMessagePreview",
+                table: "Conversation",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastMessagePreview",
+                table: "Conversation",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+        }
+    }
+}

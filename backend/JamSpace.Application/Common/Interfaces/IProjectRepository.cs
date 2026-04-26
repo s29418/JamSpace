14643359e2a0e627ff75354e9b0a1ac197e401c2
s29418 <s29418@pjwstk.edu.pl>
2026-04-26 10:44:34 +0200

    backend (feat): add ProjectRepository with GetByTeamId and AddAsync methods

--- backend/JamSpace.Application/Common/Interfaces/IProjectRepository.cs
+++ backend/JamSpace.Application/Common/Interfaces/IProjectRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace JamSpace.Application.Common.Interfaces;
+﻿using JamSpace.Domain.Entities;
+
+namespace JamSpace.Application.Common.Interfaces;
 
 public interface IProjectRepository
 {
-    
+    Task<IReadOnlyList<Project>> GetByTeamId(Guid teamId, CancellationToken ct);
+    Task AddAsync(Project project, CancellationToken ct);
 }
--- backend/JamSpace.Infrastructure/Repositories/ProjectRepository.cs
+++ backend/JamSpace.Infrastructure/Repositories/ProjectRepository.cs
@@ -1,8 +1,28 @@
 ﻿using JamSpace.Application.Common.Interfaces;
+using JamSpace.Domain.Entities;
+using JamSpace.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace JamSpace.Infrastructure.Repositories;
 
 public class ProjectRepository : IProjectRepository
 {
-    
+    private readonly JamSpaceDbContext _db;
+
+    public ProjectRepository(JamSpaceDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IReadOnlyList<Project>> GetByTeamId(Guid teamId, CancellationToken ct)
+    {
+        return await _db.Projects
+            .Where(p => p.TeamId == teamId)
+            .ToListAsync(ct);
+    }
+
+    public async Task AddAsync(Project project, CancellationToken ct)
+    {
+        await _db.Projects.AddAsync(project, ct);
+    }
 }

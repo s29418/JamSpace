46be72ccdb10674f8341fdb8f6af1436ca7dd7a8
s29418 <s29418@pjwstk.edu.pl>
2026-04-26 11:47:20 +0200

    backend (feat): add Project Controller with GetTeamProjects, CreateProject and UpdateProjectPicture endpoints

--- backend/JamSpace.API/Controllers/ProjectController.cs
+++ backend/JamSpace.API/Controllers/ProjectController.cs
@@ -1,10 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JamSpace.API.Extensions;
+using JamSpace.API.Requests;
+using JamSpace.Application.Features.Projects.Commands.Create;
+using JamSpace.Application.Features.Projects.Commands.UploadProjectPicture;
+using JamSpace.Application.Features.Projects.DTOs;
+using JamSpace.Application.Features.Projects.Queries.GetTeamProjects;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JamSpace.API.Controllers;
 
 [ApiController]
-[Route("api/teams/{id}/projects")]
+[Route("api/teams/{teamId}/projects")]
 public class ProjectController : ControllerBase
 {
-    
+    private readonly IMediator _mediator;
+    public ProjectController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<IReadOnlyList<ProjectDto>>> GetTeamProjects([FromRoute] Guid teamId, 
+        CancellationToken ct)
+    {
+        var userId = User.GetUserId();
+        var result = await _mediator.Send(new GetTeamProjectsQuery(userId, teamId), ct);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<ProjectDto>> CreateProject([FromRoute] Guid teamId, [FromBody] string name,
+        CancellationToken ct = default)
+    {
+        var result = await _mediator.Send(new CreateProjectCommand(teamId, name), ct);
+        return Ok(result);
+    }
+
+    [HttpPatch("{projectId}/picture")]
+    [Authorize]
+    public async Task<ActionResult<string>> UpdateProjectPicture([FromRoute] Guid projectId, [FromRoute] Guid teamId,
+        [FromForm] UploadPictureRequest request, CancellationToken ct = default)
+    {
+        var userId = User.GetUserId();
+
+        if (request.File is null)
+            return BadRequest("File is required.");
+
+        var url = await _mediator.Send(
+            new UploadProjectPictureCommand(projectId, teamId, userId, request.File.ToFileUpload()), ct);
+
+        return Ok(new { url });
+    }
 }
